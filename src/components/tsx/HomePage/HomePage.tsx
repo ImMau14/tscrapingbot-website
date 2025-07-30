62388ac7c7c79a57c0c911c4b1a0841b47b7a8ae
ImMau14 <newmauuelenoski02@gmail.com>
2025-07-30 02:14:39 -0400

    Style: Mobile styles fixed and changed the GitHub/TScrapingBot link to the correct path

--- src/components/tsx/HomePage/HomePage.tsx
+++ src/components/tsx/HomePage/HomePage.tsx
@@ -2,30 +2,32 @@ import { motion } from 'framer-motion'
 import { TSLogoSvg } from '@components/tsx/TSLogoSvg.tsx'
 
 const pageLinks = {
-  GitHub: 'https://github.com/ImMau14/tscrapingbot-website',
+  GitHub: 'https://github.com/ImMau14/tscrapingbot',
   Telegram: 'https://t.me/TScrapingBot'
 }
 
 // TODO: Add multilingual support.
 export const HomePage = ({className}: string) => {
+  const offset = -3
+
   return (
     <main className={ className } > 
       <motion.header
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
+        initial={{ opacity: 0, y: offset }}
+        animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, ease: 'easeOut', delay: 0 }}
         className="flex flex-col gap-4 items-start"
       >
         <TSLogoSvg className="fill-silver-300 h-20" />
         <h1 className="
-          flex items-center pr-4 relative 
-          text-5xl font-heading font-bold text-silver-300 
+          flex items-center pr-0 md:pr-4 relative 
+          text-[2.8rem] md:text-5xl font-heading font-bold text-silver-300 
           after:content-[''] after:absolute 
-          after:-bottom-3 after:right-5 
+          after:-bottom-0 md:after:-bottom-3 after:right-1 md:after:right-5
           after:h-0.5 after:w-32 after:rounded-full 
           after:bg-green-450
           before:content-['ImMau14'] before:absolute 
-          before:-bottom-4 before:left-2
+          before:-bottom-2 md:before:-bottom-4 before:left-2
           before:text-sm
         ">
           TScraping<span className="text-green-450">Bot</span>
@@ -33,24 +35,24 @@ export const HomePage = ({className}: string) => {
       </motion.header>
 
       <motion.p
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
+        initial={{ opacity: 0, y: offset }}
+        animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, ease: 'easeOut', delay: 0.2 }}
         className="text-silver-450 font-body text-sm"
       >
-        Analíza páginas con IA desde Telegram con TScrapingBot
+        Analiza páginas con IA desde Telegram con TScrapingBot
       </motion.p>
 
       <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
+        initial={{ opacity: 0, y: offset }}
+        animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, ease: 'easeOut', delay: 0.4 }}
         className="text-white flex gap-4 justify-center"
       >
         <a href={ pageLinks.GitHub } >
           <motion.button
             className="
-              flex items-center justify-center text-sm font-body py-1 px-4 
+              flex items-center justify-center text-[0.7rem] md:text-sm font-body py-1 px-4 
               hover:bg-purple-850 duration-200
               bg-purple-950 border-2 
               border-purple-300/50
@@ -67,7 +69,7 @@ export const HomePage = ({className}: string) => {
         <a href={ pageLinks.Telegram } >
           <motion.button 
             className="
-              flex items-center justify-center text-sm font-body py-1 px-4  
+              flex items-center justify-center text-[0.7rem] md:text-sm font-body py-1 px-4  
               hover:bg-green-850 duration-200
               bg-purple-950 border-2 
               border-green-300/50 
